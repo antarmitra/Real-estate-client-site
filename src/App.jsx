558868e0828ad2bb2587470446ef231f82e26307
AntarProgrammer <antarmitra33@gmail.com>
2023-11-28 21:47:42 +0600

    Basic setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-5xl font-bold bg-orange-500 text-white'>Welcome to My Website</h1>
+      <button className="btn btn-outline">Default</button>
+      <button className="btn btn-outline btn-primary">Primary</button>
+      <button className="btn btn-outline btn-secondary">Secondary</button>
+      <button className="btn btn-outline btn-accent">Accent</button>
+    </>
+  )
+}
+
+export default App
